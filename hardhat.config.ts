17ff703624040efc3a068379fe4e4ee165b1da42
lvl99wzrd <marco.elissa@gmail.com>
2025-04-09 10:42:50 +0700

    add Holesky network configuration and Etherscan API key support in Hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,13 +1,27 @@
 import { HardhatUserConfig } from "hardhat/config"
+import { vars } from "hardhat/config"
 import "@nomicfoundation/hardhat-toolbox"
 import "@openzeppelin/hardhat-upgrades"
 import "hardhat-dependency-compiler"
+
+const PRIVATE_KEY = vars.get("PRIVATE_KEY")
+const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY")
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
   networks: {
     hardhat: {
       allowUnlimitedContractSize: true,
+    },
+    holesky: {
+      chainId: 17000,
+      url: "https://1rpc.io/holesky",
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      holesky: ETHERSCAN_API_KEY,
     },
   },
   dependencyCompiler: {
